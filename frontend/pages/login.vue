65491dbac35b3fb108d29101dce6a821d7422620
widycodes <77587662+widycodes@users.noreply.github.com>
2024-09-14 21:52:19 +0200

    sla/ola ui

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="mx-auto my-32 max-w-80">
         <div class="relative my-6 w-full">
-            <input type="text" id="employeeId" 
+            <input type="text" id="employeeId" v-model="employeeId"
                 class="border-b-2 py-1 w-full focus:outline-none focus:border-[#007AFF] focus:border-b-2 transition-colors duration-500 peer" autocomplete="off">
             <label for="employeeId" 
                 class="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-[#007AFF] transition-all">Employee ID</label>
   </div>
 
   <div class="relative my-6 w-full">
-    <input type="password" id="password" 
+    <input type="password" id="password" v-model="password" 
         class="border-b-2 py-1 w-full focus:outline-none focus:border-[#007AFF] focus:border-b-2 transition-colors duration-500 peer" autocomplete="off">
     <label for="password" 
         class="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-[#007AFF] transition-all">Password</label>
   </div>
 
-  <button class="my-3 px-4 py-2 w-full bg-[#007AFF] text-white rounded-full">Login</button>
+  <button @click="onClickLogin"
+   class="my-3 px-4 py-2 w-full bg-[#007AFF] text-white rounded-full">Login</button>
     </div>
 </template>
 
@@ -22,6 +23,11 @@
     definePageMeta({
         layout: 'login'
     })
+
+    const password = ref("")
+    const employeeId = ref("")
+
+    const onClickLogin = async() => { await navigateTo('/dashboard')}
 
 </script>
 
