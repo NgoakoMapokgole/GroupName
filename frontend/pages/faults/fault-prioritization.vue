2f0987bc4536c4e7eee1152920d257aef97ccafb
Ngoako Mapokgole <100357050+NgoakoMapokgole@users.noreply.github.com>
2024-09-14 22:43:51 +0200

    Update fault-prioritization.vue

--- frontend/pages/faults/fault-prioritization.vue
+++ frontend/pages/faults/fault-prioritization.vue
@@ -1,19 +1,7 @@
 <template>
     <div class="text-lg font-semibold text-left p-4">Fault Prioritization</div>
     <body>
-    <header>
-        <nuxt-link to="" class="HeadButtons" style="float: left;" ><img src="menu.png" alt="Menu" id="menu" align="center"></nuxt-link>
-        <h1><a href="dash.html">Neural Nexus</nuxt-link></h1>
-        <nav>
-            <ul >
-                <li style="list-style: none;" ><nuxt-link to=""><img src="search.png" alt="Search" class=HeadButtons></nuxt-link></li style="list-style: none;">
     
-                <li style="list-style: none;" ><nuxt-link to=""><img src="settings.png" alt="Settings" class=HeadButtons></nuxt-link></li style="list-style: none;">
-                
-                <li style="list-style: none;" ><nuxt-link to=""><img src="profile.png" alt="Profile" class=HeadButtons></nuxt-link></li style="list-style: none;">
-            </ul>
-        </nav>
-    </header>
         <h2>Priorities</h2>
         
     <table>
