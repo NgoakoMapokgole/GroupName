d68a9f85b0071fb4c4c96c159dbeff069849c186
Ngoako Mapokgole <100357050+NgoakoMapokgole@users.noreply.github.com>
2024-09-15 15:44:03 +0200

    Create UserReport.vue

---
+++ frontend/pages/UserReport.vue
@@ -0,0 +1,37 @@
+<template>
+    <h2>Report Resolved issue And User Log</h2>
+    <form align="center">
+        <section>
+        <label for="issue">Issue</label>
+        <input type="text" name="issue" id="issue"><br><br>
+        <label for="resolved">Resolved</label>
+        <input type="text" name="resolved" id="resolved"><br><br>
+        <label for="user">User</label>
+        <input type="text" name="user" id="user"><br><br>
+        <label for="log">Log</label>
+        <input type="text" name="log" id="log"><br><br>
+        <label for="issue      s">Issue</label>
+        <input type="text" name="issue" id="issue"><br><br>
+        <label for="resolved">Resolved</label>
+        <input type="text" name="resolved" id="resolved"><br><br>
+        <label for="user">User</label>
+        <input type="text" name="user" id="user"><br><br>
+        <label for="log">Log</label>
+        <input type="text" name="log" id="log"><br><br>
+        <br><br>
+        <button type="submit">Submit</button>
+        </section>
+        
+    </form>
+    <footer align="center">
+        <p>Neural Nexus</p>
+    </footer>
+</template>
+
+<script scoped>
+
+</script>
+
+<style scoped>
+  
+</style>
