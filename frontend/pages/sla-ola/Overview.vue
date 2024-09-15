f33418d78fcd9295c6765d607f93040158e49b9d
Ngoako Mapokgole <100357050+NgoakoMapokgole@users.noreply.github.com>
2024-09-15 21:29:20 +0200

    Update and rename ola-management.vue to Overview.vue

---
+++ frontend/pages/sla-ola/Overview.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="text-lg font-semibold text-left p-4">Overview</div>
+    <body>
+    <h2>Overview</h2>
+    <section id="Details">
+        <h3>General analysis by AI</h3>
+        <h3>Overview</h3>
+        <a href=""><img src="" alt="" class="NavButton"></a>
+        <a href=""><img src="" alt="" class="NavButton"></a>
+        <a href=""><img src="" alt="" class="NavButton"></a>
+        <a href=""><img src="" alt="" class="NavButton"></a>
+        <a href=""><img src="" alt="" class="NavButton"></a>
+        <P></P>
+        <section>
+            <h4>Average Resolution time</h4>
+            <p></p>
+            <h4>Number of tickets resolved per issue </h4>
+            <p></p>
+            <h4>rate of resolution</h4>
+            <p></p>
+        </section>
+    </section>
+    <section id="Data" >
+        <section>
+            <h3>sla/ola</h3>
+            <p></p>
+            <!--graph-->
+        </section>
+       
+        <section>
+            <h3>sla/ola</h3>
+            <p></p>
+            <!--graph-->
+        </section>
+        <section>
+            <h3>sla/ola</h3>
+            <p></p>
+            <!--graph-->
+        </section>
+        <section>
+            <h3>sla/ola</h3>
+            <p></p>
+            <!--graph-->
+        </section>
+        <section>
+            <h3>sla/ola</h3>
+            <p></p>
+            <!--graph-->
+        </section>
+        
+        
+    </section>
+    <footer align="center">
+        <br>
+        <p>Neural Nexus generated statistics</p>
+    </footer>
+</body>
+
+
+
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
