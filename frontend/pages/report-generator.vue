b08b34da42e895e31d487d3c8fd3fea8a9b06bfe
Ngoako Mapokgole <100357050+NgoakoMapokgole@users.noreply.github.com>
2024-09-15 21:08:56 +0200

    Update report-generator.vue

--- frontend/pages/report-generator.vue
+++ frontend/pages/report-generator.vue
@@ -1,5 +1,47 @@
 <template>
     <div class="text-lg font-semibold text-left p-4">Report generator</div>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="cssMain.css">
+    <link rel="stylesheet" href="cssAIReport.css">
+</head>
+<body>
+    <h2>Overall Database</h2>
+    <table>
+        <thead>
+            <tr>
+                <th style="width:60px">Priority number</th>
+                <th>Ticket ID</th>
+                <th>Ticket Status</th>
+                <th>Closure Time</th>
+                <th>Title</th>
+                <th>Operator(Create TT)</th>
+                <th>CreateTime(Create TT)</th>
+                <th>Fault Level(Create TT)</th>
+                <th>Alarm Type(Create TT)</th>
+                <th>Alarm Name(Create TT)</th>
+                <th>Fault Occurrence Time(Create TT_proccsrfaultoccurtime)</th>
+                <th>SLA Status</th>
+                <th>State(Process TT)</th>
+                <th>Fault Recovery Time(Process TT_recoverytime)</th>
+                <th>Fault Resolving Time(Process TT_faultresolvingtime)</th>
+                <th>Fault last Occur Time (process)</th>
+                <th>Fault Recovery Time(Process TT)</th>
+                <th>CreateTime(Process TT)</th>
+                <th>is Handle(Handle TT)</th>
+                <th>AI analysis</th>
+                <th>AI full analysis</th>
+            </tr>
+        </thead>
+    </table>
+    <footer>
+        <p>Neural Nexus</p>
+    </footer>
+</body>
+</html>
 </template>
 
 <script setup>
